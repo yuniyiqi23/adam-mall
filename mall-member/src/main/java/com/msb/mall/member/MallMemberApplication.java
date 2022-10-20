7bb002901dc2569e3fafc705217b5d53662870ea
Adam <7450214+adam_smith_yep@user.noreply.gitee.com>
2022-10-20 21:54:53 +0800

    add application.yml

--- mall-member/src/main/java/com/msb/mall/member/MallMemberApplication.java
+++ mall-member/src/main/java/com/msb/mall/member/MallMemberApplication.java
@@ -1,9 +1,11 @@
 package com.msb.mall.member;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.msb.mall.member.dao")
 public class MallMemberApplication {
 
     public static void main(String[] args) {
--- mall-ware/src/main/java/com/msb/mall/ware/MallWareApplication.java
+++ mall-ware/src/main/java/com/msb/mall/ware/MallWareApplication.java
@@ -1,9 +1,11 @@
 package com.msb.mall.ware;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.msb.mall.ware.dao")
 public class MallWareApplication {
 
     public static void main(String[] args) {
