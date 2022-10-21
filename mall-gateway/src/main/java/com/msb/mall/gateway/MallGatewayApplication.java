5e632880b3dbf939b332584e1dea83a96b17ba86
Adam <7450214+adam_smith_yep@user.noreply.gitee.com>
2022-10-21 20:58:22 +0800

    fix gateway服务

--- mall-gateway/src/main/java/com/msb/mall/gateway/MallGatewayApplication.java
+++ mall-gateway/src/main/java/com/msb/mall/gateway/MallGatewayApplication.java
@@ -1,17 +1,15 @@
-package com.msb.mall.coupon;
+package com.msb.mall.gateway;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @EnableDiscoveryClient
 @SpringBootApplication
-@MapperScan("com.msb.mall.coupon.dao")
-public class MallCouponApplication {
+public class MallGatewayApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(MallCouponApplication.class, args);
+        SpringApplication.run(MallGatewayApplication.class, args);
     }
 
 }
